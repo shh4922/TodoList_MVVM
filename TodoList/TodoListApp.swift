cdd7ef431dd77897bab1c2aff775974e62453db5
sinhyeonho <gusgh4922@gmail.com>
2023-01-02 22:35:53 +0900

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by HyeonHo on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
